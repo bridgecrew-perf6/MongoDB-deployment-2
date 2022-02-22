6e17b08e655a5c7db621d1c25a0df88b70d828c7
kagopooe <kagopooe@gmail.com>
2022-02-22 09:12:51 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+
+const express = require('express')
+const req = require('express/lib/request')
+const { default: mongoose } = require('mongoose')
+const app = express()
+
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers', subscribersRouter)
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// app.listen(3000, () => console.log('Server Started'))
